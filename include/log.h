7cd6cec532e12841f2fc1dee9a90e2fa5b651736
zejiedu <duzejie@hotmail.com>
2019-02-01 13:54:44 +0800

    log

--- include/log.h
+++ include/log.h
@@ -6,29 +6,31 @@
 class DUZEJIE_LOG
 {
 private:        
-    bool out=false;
+    bool out;
+    bool quit;
+    std::ostream &o;
 public:
     template<class T>
     DUZEJIE_LOG & operator << (T t){        
-        if (out) {               
-            std::cout <<t<<" ";  
-        }
+        if (out)           
+            o <<t<<" ";         
         return *this;
     }
-    DUZEJIE_LOG(bool test):out(!test){   } 
-    ~DUZEJIE_LOG(){
-        if (out) {                       
-            std::cout <<std ::endl;
-            exit(0);       ///  
-        }
+    DUZEJIE_LOG(bool test, bool isQuit, std::ostream &os):out(test), quit(isQuit), o(os){   } 
+    ~DUZEJIE_LOG(){      
+        if(quit)
+            exit(0);       ///          
     }
 };
 
+static std::string  getCurrentTimeStr(){
+	time_t t = time(NULL);
+	char ch[64] = {0};
+	strftime(ch, sizeof(ch) - 1, "[%Y-%m-%d %H:%M:%S]", localtime(&t));     //年-月-日 时-分-秒
+	return ch;
+}
 
-
-#define CHECK(x)  if (!(x)) { std::cout<<std::endl;std::cout<< __FILE__<<" at line " <<__LINE__<< "  Check failed. "; } DUZEJIE_LOG((x))
-//#define CHECK(x)     if (!(x)) { std::cout<<std::endl;std::cout<< __FILE__<<" at line " <<__LINE__<< "  Check failed"<<std ::endl; exit(0); } std::cout
-#define INFO       std::cout<<std::endl; std::cout<< __FILE__<<" at line " <<__LINE__<< "  INFO:" ; std::cout
-
-
-
+#define CHECK(x)       if (!(x)){ std::cerr<<std::endl;std::cerr<< getCurrentTimeStr() <<"CHECK failed: "<<__FILE__<<", at line:" <<__LINE__<< ". "; } DUZEJIE_LOG(!(x), !(x),std::cerr)
+#define INFO                      std::clog<<std::endl;std::clog<< getCurrentTimeStr() <<"INFO:         "<<__FILE__<<", at line:" <<__LINE__<< ". ";   std::clog
+#define WARNING(x)     if (!(x)){ std::clog<<std::endl;std::clog<< getCurrentTimeStr() <<"WARNING:      "<<__FILE__<<", at line:" <<__LINE__<< ". "; } DUZEJIE_LOG(!(x), false, std::clog)   
+#define CAUTION                   std::clog<<std::endl;std::clog<< getCurrentTimeStr() <<"CAUTION:      "<<__FILE__<<", at line:" <<__LINE__<< ". ";   DUZEJIE_LOG(true, false, std::clog)   

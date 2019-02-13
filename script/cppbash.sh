d61dab16859bccfc8cbe5dd5d09cc510be900af0
duzejie <duzejie@hotmail.com>
2019-02-13 09:47:12 +0800

    opencv cpp alias

--- script/cppbash.sh
+++ script/cppbash.sh
@@ -3,24 +3,25 @@
 
 
 #在PATH中找到可执行文件程序的路径。
-export PATH =$PATH:$HOME/bin  
+export PATHoo=$PATH:$HOME/bin  
 
 #gcc找到头文件的路径
-C_INCLUDE_PATH=$C_INCLUDE_PATH:/MyLib  
+C_INCLUDE_PATH=$C_INCLUDE_PATH:/usr/include/opencv4  
 export C_INCLUDE_PATH  
 
 #g++找到头文件的路径
-CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:/MyLib  
+CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:/usr/include/opencv4  
 export CPLUS_INCLUDE_PATH
 
 
 #找到动态链接库的路径
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/MyLib  
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib  
 export LD_LIBRARY_PATH  
 
 #找到静态库的路径
-LIBRARY_PATH=$LIBRARY_PATH:/MyLib  
+LIBRARY_PATH=$LIBRARY_PATH:/usr/lib   
 export LIBRARY_PATH  
+
 
 
 #下面是在gcc命令中手动设置搜索路径：
@@ -31,5 +32,16 @@ export LIBRARY_PATH
 #添加静态库搜索路径
 # gcc foo.c -L /home/xiaowp/lib -static -lfoo -o foo 
 
+
+
+
+
+
+#alias copencv_static="g++ -static -ggdb -I/usr/include/opencv4 -L/usr/lib -lpthread -lopencv_shape -lopencv_stitching -lopencv_objdetect -lopencv_superres -lopencv_videostab -lopencv_calib3d -lopencv_features2d -lopencv_highgui -lopencv_videoio -lopencv_imgcodecs -lopencv_video -lopencv_photo -lopencv_ml -lopencv_imgproc -lopencv_flann -lopencv_core"
+
+alias 2e_opencv="g++  -ggdb -Wall -I/usr/include/opencv4 -L/usr/lib -lpthread -lopencv_shape -lopencv_stitching -lopencv_objdetect -lopencv_superres -lopencv_videostab -lopencv_calib3d -lopencv_features2d -lopencv_highgui -lopencv_videoio -lopencv_imgcodecs -lopencv_video -lopencv_photo -lopencv_ml -lopencv_imgproc -lopencv_flann -lopencv_core"
+alias 2e_opencvl="g++  -ggdb -Wall -I/usr/include/opencv4 -L/usr/lib -lopencv_highgui -lopencv_videoio -lopencv_imgcodecs -lopencv_video -lopencv_photo  -lopencv_imgproc  -lopencv_core"
+
+alias 2e_opencv_r="g++ -I/usr/include/opencv4 -L/usr/lib -lpthread -lopencv_shape -lopencv_stitching -lopencv_objdetect -lopencv_superres -lopencv_videostab -lopencv_calib3d -lopencv_features2d -lopencv_highgui -lopencv_videoio -lopencv_imgcodecs -lopencv_video -lopencv_photo -lopencv_ml -lopencv_imgproc -lopencv_flann -lopencv_core"
 
 
